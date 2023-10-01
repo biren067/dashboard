a9a63c36d88c9450db6990f36bc9f1195f90208f
Birendra kumar <birendrabitian@gmail.com>
2023-10-01 08:21:04 +0530

    bond

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
+import HomePage from '@/components/HomePage'
 
 export default function Home() {
   return (
-    <main>
-      Bond
-    </main>
+    <HomePage/>
+
   )
 }
